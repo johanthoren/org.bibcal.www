934e628e23f320650d7180eb11ccf151275b8f8a
Johan Thoren <johan@thoren.xyz>
2021-09-08 22:23:21 +0200

    Version 0.5.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.bibcal/www "0.5.5"
+(defproject org.bibcal/www "0.5.6-SNAPSHOT"
   :description "Public website at www.bibcal.org"
   :url "https://www.bibcal.org"
   :license {:name "ISC"
